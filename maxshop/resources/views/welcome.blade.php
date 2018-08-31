db73788b4839df07e867566f1be72425da4e4393
tiennm-vinsofts <tiennm@vinsofts.net>
2018-08-31 09:46:20 +0700

    add folder

--- maxshop/resources/views/welcome.blade.php
+++ maxshop/resources/views/welcome.blade.php
@@ -47,24 +47,24 @@
         <script type="text/javascript" src="{{asset('js/app.js')}}"></script>
         <script src="js/jquery.min.js"></script>
 
-	<!-- Library - Js -->
-	<script src="libraries/lib.js"></script>
-	
-	<script src="libraries/jquery.countdown.min.js"></script>
-	
-	<!-- RS5.0 Core JS Files -->
-	<script type="text/javascript" src="revolution/js/jquery.themepunch.tools.min.js?rev=5.0"></script>
-	<script type="text/javascript" src="revolution/js/jquery.themepunch.revolution.min.js?rev=5.0"></script>
-	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.video.min.js"></script>
-	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
-	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
-	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
-	<script type="text/javascript" src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
-	
-	<!-- Library - Google Map API -->
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDW40y4kdsjsz714OVTvrw7woVCpD8EbLE"></script>
+        <!-- Library - Js -->
+        <script src="libraries/lib.js"></script>
+        
+        <script src="libraries/jquery.countdown.min.js"></script>
+        
+        <!-- RS5.0 Core JS Files -->
+        <script type="text/javascript" src="revolution/js/jquery.themepunch.tools.min.js?rev=5.0"></script>
+        <script type="text/javascript" src="revolution/js/jquery.themepunch.revolution.min.js?rev=5.0"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.video.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
+        
+        <!-- Library - Google Map API -->
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDW40y4kdsjsz714OVTvrw7woVCpD8EbLE"></script>
 
-	<!-- Library - Theme JS -->
-	<script src="js/functions.js"></script>
+        <!-- Library - Theme JS -->
+        <script src="js/functions.js"></script>
     </body>
 </html>
