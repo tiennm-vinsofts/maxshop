f8dc929d69f3ecca340e3bdd98327e47a3a40c05
Lê Thanh Tùng <tungit.bkap@gmail.com>
2018-09-05 09:03:59 +0700

    config

--- maxshop/resources/assets/js/config.js
+++ maxshop/resources/assets/js/config.js
@@ -1,5 +1,5 @@
 const config = {
     urlsever: "http://192.168.1.23/maxshop/maxshop/public/",
-    urlapi: "http://192.168.1.23/maxshop/maxshop/public/index.php/api/",
+    urlapi: "http://192.168.1.23/maxshop/maxshop/public/index.php/api/"
 };
 export default config;
